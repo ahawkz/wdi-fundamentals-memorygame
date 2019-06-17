86171696c19f20f09c99895c66500533c407e634
Alex Hawkins <alexhawkins@Alexs-MacBook-Pro.local>
2019-06-17 12:17:35 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,20 +2,34 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
+/*var cardOne = cards[0];
 cardsInPlay.push(cardOne);
-cardsInPlay;
+cardsInPlay;*/
 
-var cardTwo = cards[2];
-
+/*var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
-cardsInPlay;
+cardsInPlay;*/
 
+function checkForMatch () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
 if (cardsInPlay.length === 2) {
+	checkForMatch;
  if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert('You found a match!');
 } else {
 	alert('Sorry, try again.');
 }
 }
+};
+
+flipCard(0);
+flipCard(2);
